1105a3bb745d9252d9f75611945e4ea88a5d7708
unknown <jakub.wisniewski96@gmail.com>
2017-11-19 15:59:15 +0100

    pouch db fetching data

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,72 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import ReactTable from "react-table"
+import PouchDB from "pouchdb"
 
 import "react-table/react-table.css"
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        let db = new PouchDB("books")
         
+        // db.bulkDocs([{
+        //     "title": "Krzyżacy",
+        //     "author": "Henryk Sienkiewicz"
+        // }, {
+        //     "title": "Dziady",
+        //     "author": "Adam Mickiewicz"
+        // }]).then( result => {
+        //     console.log(result)
+        // })
+        
+        db.allDocs({
+            include_docs: true
+        }).then( result => {
+            let docs = result.rows.map( row => {
+                return row.doc
+            })
+
+            this.setState({data: docs})
+        })     
+    }
+
+    render() {
         const columns = [{
-            Header: "L.p.",
-            accessor: "id"
+            Header: "ID",
+            accessor: "_id"
         }, {
             Header: "Tytuł",
             accessor: "title"
         }, {
             Header: "Autor",
             accessor: "author"
-        }]
-
+        }, {
+            Header: "Ocena",
+            accessor: "rating"
+        }, {
+            Header: "Opis",
+            accessor: "description"
+        }] 
+        
         return (
             <ReactTable
-                data={data}
+                data={this.state.data}
                 columns={columns}
                 
-                showPaginationTop={true}
                 showPaginationBottom={true}
 
                 filterable={true}
                 defaultFilterMethod={(filter, row, column) => {
                     const id = filter.pivotId || filter.id
-                    return row[id] !== undefined ? String(row[id]).includes(filter.value) : true
+                    return row[id] !== undefined ? String(row[id]).toUpperCase().includes(filter.value.toUpperCase()) : true
                 }}
             />
         )
