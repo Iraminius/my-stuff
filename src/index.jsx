5f80ecda58763acfc31dd61dba214b63d7e80f21
unknown <jakub.wisniewski96@gmail.com>
2017-11-19 13:45:36 +0100

    simple table

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,38 @@
-var React = require("react")
-var ReactDOM = require("react-dom")
- 
+import React from "react"
+import ReactDOM from "react-dom"
+import ReactTable from "react-table"
+
+import "react-table/react-table.css"
+
 class App extends React.Component {
     render() {
-        return <div>My Stuff</div>
+        
+        const columns = [{
+            Header: "L.p.",
+            accessor: "id"
+        }, {
+            Header: "Tytuł",
+            accessor: "title"
+        }, {
+            Header: "Autor",
+            accessor: "author"
+        }]
+
+        return (
+            <ReactTable
+                data={data}
+                columns={columns}
+                
+                showPaginationTop={true}
+                showPaginationBottom={true}
+
+                filterable={true}
+                defaultFilterMethod={(filter, row, column) => {
+                    const id = filter.pivotId || filter.id
+                    return row[id] !== undefined ? String(row[id]).includes(filter.value) : true
+                }}
+            />
+        )
     }
 }
  
